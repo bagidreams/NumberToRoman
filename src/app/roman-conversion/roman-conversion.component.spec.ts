dc5d7afe3f0fd764e99ceb2a7264384f117d6fb0
angularproject2019 <56632494+angularproject2019@users.noreply.github.com>
2019-10-21 16:38:41 +0530

    test cases for convertToRoman

--- src/app/roman-conversion/roman-conversion.component.spec.ts
+++ src/app/roman-conversion/roman-conversion.component.spec.ts
@@ -22,4 +22,18 @@ describe('RomanConversionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should 66 be converted to LXVI', () => {    
+   expect(Component.convertToRoman(66)).toBe('LXVI');
+  });
+  it('should 530 be converted to DXXX', () => {    
+   expect(Component.convertToRoman(530)).toBe('DXXX');
+  });
+   it('should 39 be converted to XXXIX', () => {    
+   expect(Component.convertToRoman(39)).toBe('XXXIX');
+  });
+   it('should 97 be converted to XCVII', () => {    
+   expect(Component.convertToRoman(97)).toBe('XCVII');
+  });
+  
 });
